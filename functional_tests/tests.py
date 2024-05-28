3a4b86a6d7c9a17f7093118fce2d4c1af2da606b
Jennifer Saikumar <jennifersonia.s@gmail.com>
2024-05-28 09:17:31 +0100

    make functional_tests app use LiverServerTestCase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,3 +1,4 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver 
 from selenium.webdriver.common.by import By 
 from selenium.webdriver.common.keys import Keys
@@ -5,7 +6,7 @@ import time
 import unittest 
 
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
 
@@ -13,18 +14,18 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.quit()
 
     def check_for_row_in_list_table(self, row_text):
-      table = self.broswer.find_element(By.ID, "id_list_table")
+      table = self.browser.find_element(By.ID, "id_list_table")
       rows = table.find_elements(By.TAG_NAME, "tr")
       self.assertIn(row_text, [row.text for row in rows])  
 
     def test_can_start_a_todo_list(self):
         # Edith has heard about a cool new online to-do app. 
         # She goes to check out its homepage 
-        self.browser.get("http://localhost:8000")
+        self.browser.get(self.live_server_url)
 
         # She notices the page title and header mention to-do lists 
         self.assertIn("To-Do", self.browser.title)
-        header_text = self.broswer.find_element(By. TAG_NAME, "h1").text 
+        header_text = self.browser.find_element(By. TAG_NAME, "h1").text 
         self.assertIn("To-Do", header_text)
 
         # She is invited to enter a to-do item straight away 
@@ -33,18 +34,21 @@ class NewVisitorTest(unittest.TestCase):
 
         # She types "Buy peacock feathers" into a text box
         # (Edith's hobby is tying fly-fishing lures)
-        input.send_keys("Buy peacock feathers")
+        inputbox.send_keys("Buy peacock feathers")
 
         # When she hits enter, the page updates, and now the page lists 
         # "1: Buy peacock feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
+
+        table = self.browser.find_element(By.ID, "id_list_table")
+        rows = table.find_elements(By.TAG_NAME, "tr")
         self.check_for_row_in_list_table("1: Buy peacock feathers")
 
         # There is still a text box inviting her to add another item. 
         # She enters "Use peacock feathers to make a fly" 
         # (Edith is very methodical)
-        inputbox = self.broswer.find_element(By.ID, "id_new_item")
+        inputbox = self.browser.find_element(By.ID, "id_new_item")
         inputbox.send_keys("Use peacock feathers to make a fly")
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
