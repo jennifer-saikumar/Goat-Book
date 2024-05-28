6f319d4eac1d80d9a0f8369931107a72710cdba2
Jennifer Saikumar <jennifersonia.s@gmail.com>
2024-05-28 09:49:16 +0100

    ensuring test isolation and removing voodoo sleeps

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -5,6 +5,7 @@ from selenium.webdriver.common.keys import Keys
 import time 
 import unittest 
 
+MAX_WAIT = 5
 
 class NewVisitorTest(LiveServerTestCase):
     def setUp(self):
@@ -13,10 +14,19 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def check_for_row_in_list_table(self, row_text):
-      table = self.browser.find_element(By.ID, "id_list_table")
-      rows = table.find_elements(By.TAG_NAME, "tr")
-      self.assertIn(row_text, [row.text for row in rows])  
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element(By.ID, "id_list_table")
+                rows = table.find_elements(By.TAG_NAME, "tr")
+                self.assertIn(row_text, [row.text for row in rows])  
+                return 
+            except (AssertionError, WebDriverException):
+                if time.time() - start_time > MAX_WAIT:
+                    raise
+                time.sleep(0.5)
+    
 
     def test_can_start_a_todo_list(self):
         # Edith has heard about a cool new online to-do app. 
@@ -43,7 +53,7 @@ class NewVisitorTest(LiveServerTestCase):
 
         table = self.browser.find_element(By.ID, "id_list_table")
         rows = table.find_elements(By.TAG_NAME, "tr")
-        self.check_for_row_in_list_table("1: Buy peacock feathers")
+        self.wait_for_row_in_list_table("1: Buy peacock feathers")
 
         # There is still a text box inviting her to add another item. 
         # She enters "Use peacock feathers to make a fly" 
@@ -54,8 +64,8 @@ class NewVisitorTest(LiveServerTestCase):
         time.sleep(1)
         
         # The page updates again, and now shows both items on her list
-        self.check_for_row_in_list_table("1: Buy peacock feathers")
-        self.check_for_row_in_list_table("2: Use peacock feathers to make a fly")
+        self.wait_for_row_in_list_table("1: Buy peacock feathers")
+        self.wait_for_row_in_list_table("2: Use peacock feathers to make a fly")
 
         # Satisfied, she goes back to sleep
 
