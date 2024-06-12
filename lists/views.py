41f513118ed5e86303ab252ba6db11d65890dd40
Jennifer Saikumar <jennifersonia.s@gmail.com>
2024-06-12 13:42:01 +0100

    A new URL and view for existing list and FT passes

--- lists/views.py
+++ lists/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import redirect, render
 from lists.models import Item, List   
 
-
 def home_page(request):
     if request.method == "POST": 
      Item.objects.create(text=request.POST["item_text"])
@@ -10,10 +9,14 @@ def home_page(request):
 
 def view_list(request, list_id):
    our_list = List.objects.get(id=list_id)
-   items = Item.objects.filter(list=our_list)
-   return render(request, "list.html", {"items": items})
+   return render(request, "list.html", {"list": our_list})
 
 def new_list(request):
    nulist = List.objects.create()
    Item.objects.create(text=request.POST["item_text"], list=nulist)
    return redirect(f"/lists/{nulist.id}/")
+
+def add_item(request, list_id): 
+   our_list = List.objects.get(id=list_id)
+   Item.objects.create(text=request.POST["item_text"], list=our_list)
+   return redirect(f"/lists/{our_list.id}/")
