1fe2124fb2c3ebab1b85089d4946000fa095d33a
Jennifer Saikumar <jennifersonia.s@gmail.com>
2024-06-12 09:28:50 +0100

    changed files and migrations adjusting models

--- lists/tests.py
+++ lists/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from lists.models import Item 
+from lists.models import Item, List 
  
 class HomePageTest(TestCase):
     def test_uses_home_template(self):
@@ -10,15 +10,23 @@ class HomePageTest(TestCase):
         self.client.get("/")
         self.assertEqual(Item.objects.count(), 0) 
         
-class ItemModelTest(TestCase):
+class ListAndItemModelsTest(TestCase):
     def test_saving_and_retrieving_items(self):
+        mylist = List()
+        mylist.save()
+
         first_item = Item()
         first_item.text = "The first (ever) list item"
+        first_item.list = mylist
         first_item.save()
 
         second_item = Item()
         second_item.text = "Item the second"
+        second_item.list = mylist
         second_item.save()
+
+        saved_list = List.objects.get()
+        self.assertEqual(saved_list, mylist)
 
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
@@ -26,7 +34,9 @@ class ItemModelTest(TestCase):
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, "The first (ever) list item")
+        self.assertEqual(first_saved_item.list, mylist)
         self.assertEqual(second_saved_item.text, "Item the second")
+        self.assertEqual(second_saved_item.list, mylist)
 
     def test_home_page_returns_correct_html(self):
         response = self.client.get("/")
@@ -35,17 +45,15 @@ class ItemModelTest(TestCase):
         self.assertContains(response, "</html>")
         self.assertTemplateUsed(response, "home.html")
 
-class ListViewTest:
+class ListViewTest(TestCase):
      def test_uses_list_template(self):
          response = self.client.get("/lists/the-only-list-in-the-world/")
          self.assertTemplateUsed(response, "list.html")
 
      def test_displays_all_list_items(self): 
-        Item.objects.create(text="itemey 1")
-        Item.objects.create(text="itemey 2")
-        response = self.client.get("/lists/the-only-list-in-the-world/")
-        self.assertContains(response, "itemey 1")
-        self.assertContains(response, "itemey 2")
+         mylist = List.objects.create()
+         Item.objects.create(text="itemey 1", list=mylist)
+         Item.objects.create(text="itemey 2", list=mylist)
         
 class NewListTest(TestCase):
        def test_can_save_a_POST_request(self):
